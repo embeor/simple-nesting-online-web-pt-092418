55595194e2c4d3f4aa90f08159122fd0b1103d17
Emilio Betancourt <embeor@gmail.com>
2018-10-17 22:07:06 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -113,21 +113,7 @@ def changing_alan
      alans_new_info = "GUI"
 
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-		 programmer_hash =
-		 	{
-		 			:grace_hopper => {
-		 				:known_for => "COBOL",
-		 				:languages => ["COBOL", "FORTRAN"]
-		 			},
-		 			:alan_kay => {
-		 				:known_for => "Object Orientation",
-		 				:languages => ["Smalltalk", "LISP"]
-		 			},
-		 			:dennis_ritchie => {
-		 				:known_for => "Unix",
-		 				:languages => ["C"]
-		 			}
-		 	 }
+
 
 end
 
